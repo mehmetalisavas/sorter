a1eb34e1d01192ac1fd57d9606fba0ad5d199123
mehmetalisavas <mehmetali.savas@yandex.com>
2015-10-17 02:42:11 +0300

    insertion: insertion sorting algoritm is added

---
+++ insertion.go
@@ -0,0 +1,20 @@
+package sorter
+
+func InsertionSort(arr []int) []int {
+	var key int
+	var j int
+	var resultArr []int
+
+	for i := 1; i < len(arr); i++ {
+		key = arr[i]
+		j = i - 1
+		for j >= 0 && arr[j] > key {
+			arr[j+1] = arr[j]
+			j--
+			arr[j+1] = key
+		}
+	}
+
+	resultArr = arr
+	return resultArr
+}
