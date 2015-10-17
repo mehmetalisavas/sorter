dff7e3c167505c2fef47230920c80bce9e571527
mehmetalisavas <mehmetali.savas@yandex.com>
2015-10-17 04:33:16 +0300

    more test type is added for insertion sort

--- insertion_test.go
+++ insertion_test.go
@@ -2,13 +2,10 @@ package sorter
 
 import (
 	"fmt"
-	"path/filepath"
-	"reflect"
-	"runtime"
 	"testing"
 )
 
-func TestInsertion(t *testing.T) {
+func TestInsertionSortManual(t *testing.T) {
 	arrRandom := []int{7, 2, 5, 4, 6, 9}
 	resultArr := []int{2, 4, 5, 6, 7, 9}
 	arr := InsertionSort(arrRandom)
@@ -16,10 +13,20 @@ func TestInsertion(t *testing.T) {
 	equals(t, exp, arr)
 }
 
-func equals(tb testing.TB, exp, act interface{}) {
-	if !reflect.DeepEqual(exp, act) {
-		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
-		tb.Fail()
+func TestInsertionSort(t *testing.T) {
+	generatedArr, err := GenerateArray(8)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	arr := InsertionSort(generatedArr)
+	b, err := IsSorted(arr)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	if !b {
+		fmt.Println("Array is not sorted")
+		return
 	}
 }
