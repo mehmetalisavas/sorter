c298c9094ed87d1a308609080c5cd70b8b572978
mehmetalisavas <mehmetali.savas@yandex.com>
2015-10-18 20:34:37 +0300

    selection: selection sort is added

---
+++ selection.go
@@ -0,0 +1,20 @@
+package sorter
+
+func SelectionSort(arr []int) []int {
+	var min int
+	var temp int
+
+	for i := 0; i < len(arr)-1; i++ {
+		min = i
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[min] {
+				min = j
+			}
+		}
+		temp = arr[i]
+		arr[i] = arr[min]
+		arr[min] = temp
+
+	}
+	return arr
+}
