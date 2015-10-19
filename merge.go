5c215424630cdb8465a1564ee90a0ce8193bca5e
mehmetalisavas <mehmetali.savas@yandex.com>
2015-10-19 18:44:02 +0300

    merge: some explanation for merge sort

--- merge.go
+++ merge.go
@@ -1,5 +1,25 @@
 package sorter
 
+//
+//Divide the unsorted list into n sublists, each containing 1 element (a list of 1 element is considered sorted).
+//Repeatedly merge sublists to produce new sorted sublists until there is only 1 sublist remaining. This will be the sorted list.
+
+// MergeSort divides the unsorted list sublists that containing 1 element
+// After this process, gets these each of list that has 1 element
+// and merge sublists repeatedly to produce new sorted  sublists
+// This process will last until there is only 1 sublist remaining
+// This remaining list will be sorted list.
+//
+// In example ; Assume an array has {4,3,6,2,9,8}
+// After MergeSort called
+// we have 2 sublists that have for left {4,3,6} and for right {2,9,8}
+// List will look like this
+// 					{4,3,6,2,9,8}
+// 			{4,3,6}				{2,9,8}
+// 		{4}		{3,6}		  {2}     {9,8}
+// 		{4}	   {3} {6}		  {2}    {9}  {8}
+//---- After this section
+// Merge will work to bring together the sublists as sorted
 func MergeSort(arr []int) []int {
 	if len(arr) < 2 {
 		return arr
