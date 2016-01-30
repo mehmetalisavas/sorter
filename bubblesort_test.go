dbde9e43177fe557eaf027c6f739af69871262ed
Mehmet Ali <mehmetali.savas@yandex.com>
2016-01-30 23:42:18 +0200

    tests: comment for bubblesort tests

--- bubblesort_test.go
+++ bubblesort_test.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"testing"
 )
-
+// TestBubbleSort generates the array with 8 elements randomly
+// and checks the BubbleSort is running correctly or not
 func TestBubbleSort(t *testing.T) {
 	generatedArr, err := GenerateArray(8)
 	if err != nil {
