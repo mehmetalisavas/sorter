51835ab0b8d4613767f5d527b31d94a087104968
mehmetalisavas <mehmetali.savas@yandex.com>
2017-02-09 00:36:14 +0300

    bubblesort: use one line swap

--- bubblesort.go
+++ bubblesort.go
@@ -1,16 +1,12 @@
 package sorter
 
-// BubbleSort is popular but inefficient sorting algoritm
+// BubbleSort is popular but inefficient sorting algorithm
 // it swaps adjacent elements by repeatedly
 func BubbleSort(arr []int) []int {
-	var temp int
-
 	for i := 0; i < len(arr)-1; i++ {
 		for j := len(arr) - 1; j > i; j-- {
 			if arr[j] < arr[j-1] {
-				temp = arr[j]
-				arr[j] = arr[j-1]
-				arr[j-1] = temp
+				arr[j], arr[j-1] = arr[j-1], arr[j]
 			}
 		}
 	}
