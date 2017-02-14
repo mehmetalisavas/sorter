cafde20937b366c015590b4a12f89d9e2c1530fd
mehmetalisavas <mehmetali.savas@yandex.com>
2017-02-14 23:23:31 +0300

    tests/selection: add parallel testing for selection sort algorithm

--- selection_test.go
+++ selection_test.go
@@ -24,3 +24,42 @@ func TestSelectionSort(t *testing.T) {
 		return
 	}
 }
+
+// TestSelectionSortParallel tests the selections sort algorithm
+func TestSelectionSortParallel(t *testing.T) {
+	tests := []struct {
+		arraySize int
+		isSorted  bool
+		err       error
+	}{
+		{0, false, ErrArrayNoLength},
+		{1, true, nil},
+		{2, true, nil},
+		{3, true, nil},
+		{4, true, nil},
+		{10, true, nil},
+	}
+
+	for _, test := range tests {
+		test := test // capture variable
+		t.Run("", func(t *testing.T) {
+			t.Parallel()
+			arr, err := GenerateArray(test.arraySize)
+			if err != test.err {
+				t.Fatalf("error should be: %v, but got: %v", test.err, err)
+			}
+
+			if len(arr) != test.arraySize {
+				t.Fatalf("array length should equal: %v, but got: %v", test.arraySize, len(arr))
+			}
+
+			sorted, err := IsSorted(SelectionSort(arr))
+			if err != nil {
+				t.Fatalf("error should be nil, but got:%v", err)
+			}
+			if !sorted {
+				t.Fatalf("array should be sorted:%v", sorted)
+			}
+		})
+	}
+}
