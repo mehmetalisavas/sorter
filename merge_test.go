3250dc7781eeea4bb9a7eedf5702bc34f86babe6
mehmetalisavas <mehmetali.savas@yandex.com>
2017-02-18 20:12:53 +0300

    tests: add mergesort tests with parallelisation

--- merge_test.go
+++ merge_test.go
@@ -24,4 +24,40 @@ func TestMergeSort(t *testing.T) {
 	}
 }
 
+func TestMergeSortParallel(t *testing.T) {
+	tests := []struct {
+		arraySize int
+		isSorted  bool
+		err       error
+	}{
+		{0, false, ErrArrayNoLength},
+		{1, true, nil},
+		{2, true, nil},
+		{3, true, nil},
+		{4, true, nil},
+		{10, true, nil},
+	}
 
+	for _, test := range tests {
+		test := test // capture variable
+		t.Run("", func(t *testing.T) {
+			t.Parallel()
+			arr, err := GenerateArray(test.arraySize)
+			if err != test.err {
+				t.Fatalf("error should be: %v, but got: %v", test.err, err)
+			}
+
+			if len(arr) != test.arraySize {
+				t.Fatalf("array length should equal: %v, but got: %v", test.arraySize, len(arr))
+			}
+
+			sorted, err := IsSorted(MergeSort(arr))
+			if err != nil {
+				t.Fatalf("error should be nil, but got:%v", err)
+			}
+			if !sorted {
+				t.Fatalf("array should be sorted:%v", sorted)
+			}
+		})
+	}
+}
